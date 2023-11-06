a990086c6c1a31b697ac724363fdc77267ed3579
Usama Rahman <rahmanusama390@gmail.com>
2023-11-06 06:24:11 +0600

    !

--- 02 Guess My Number/script.js
+++ 02 Guess My Number/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const randomNumber = Math.floor(Math.random() * 20 + 1);
+const guess = document.querySelector('.guess');
+const check = document.querySelector('.check');
+console.log(randomNumber);
+console.log(guess.value);
+check.addEventListener('click', function () {
+  if (guess === randomNumber) {
+    console.log('win');
+  } else {
+    console.log('over');
+  }
+});
+// Working
