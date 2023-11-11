2ec7232925ce6c326e0e23e4c1c9b2becc968556
Usama Rahman <rahmanusama390@gmail.com>
2023-11-11 14:21:48 +0600

    1

--- 02 DataStructures/10-Functions.js
+++ 02 DataStructures/10-Functions.js
@@ -3,12 +3,55 @@
 // 10 - A Closer Look at Functions
 
 //  ---------- Index -----------
+
 // Default Parameters
 // How Passing Arguments Works Value vs. Reference
+// First-Class and Higher-Order Functions
+// Functions Accepting Callback Functions
+
 ////////////////////////////////////////
+
+////////////////////////////////////////
+
+// Functions Accepting Callback Functions
+
 ////////////////////////////////////////
 
 // How Passing Arguments Works Value vs. Reference
+/*
+const flight = 'BH423';
+const usama = {
+  name: 'Usama Rahman',
+  passport: 445426089674,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LE433';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 445426089674) {
+    console.log('Please Check In 👍');
+  } else {
+    console.log(' ⚠️ Worng Passport !! ');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(usama);
+checkIn(flight, usama);
+*/
 
 ////////////////////////////////////////
 
--- 02 DataStructures/Functions.md
+++ 02 DataStructures/Functions.md
@@ -2,9 +2,48 @@
 
 ---
 
-- [Default Parameters]()
+- [Default Parameters](#default-parameters)
 
-- ***
+- [How Passing Arguments Works]()Value vs. Reference
+
+---
+
+## How Passing Arguments Works Value vs. Reference
+
+```js
+const flight = 'BH423';
+const usama = {
+  name: 'Usama Rahman',
+  passport: 445426089674,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LE433';
+  passenger.name = 'Mr.' + passenger.name;
+
+  if (passenger.passport === 445426089674) {
+    console.log('Please Check In 👍');
+  } else {
+    console.log(' ⚠️ Worng Passport !! ');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(usama);
+checkIn(flight, usama);
+```
 
 ## Default Parameters
 
