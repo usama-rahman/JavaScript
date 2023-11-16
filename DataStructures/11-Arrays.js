ee375d16c28159dd0927ed59adb026ed7f2a3aea
Usama Rahman <rahmanusama390@gmail.com>
2023-11-16 12:18:06 +0600

    ok

--- DataStructures/11-Arrays.js
+++ DataStructures/11-Arrays.js
@@ -33,3 +33,27 @@ console.log(arr.slice());
 console.log([...arr]);
 
 // SPLICE
+
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// Reverse
+
+let arr1 = ['a', 'b', 'c', 'd', 'e'];
+let arr2 = ['j', 'i', 'h', 'g', 'f'];
+
+console.log(arr2.reverse());
+console.log(arr2);
+
+// Concat
+
+const letters = arr1.concat(arr2);
+console.log(letters);
+console.log([...arr1, ...arr2]);
+
+// Join
+
+console.log(letters.join(' - '));
