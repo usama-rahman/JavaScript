5502cf673b4355d3734b99f2e04499eb42b990b3
Usama Rahman <rahmanusama390@gmail.com>
2023-11-16 11:57:17 +0600

    ok

--- DataStructures/11-Arrays.js
+++ DataStructures/11-Arrays.js
@@ -19,3 +19,17 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 ////////////////////////////////////////
 
 // Simple Array Methods
+
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+
+console.log(arr.slice(2));
+console.log(arr.slice(2, 4));
+console.log(arr.slice(-2));
+console.log(arr.slice(-1));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE
