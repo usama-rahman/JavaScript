391b93ef0392ca08e3d4eeb48a2784c5225f0f3f
Usama Rahman <rahmanusama390@gmail.com>
2023-11-16 11:51:05 +0600

    Start

--- DataStructures/11-Arrays.js
+++ DataStructures/11-Arrays.js
@@ -7,3 +7,15 @@
 // Simple Array Methods
 
 ////////////////////////////////////////
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+////////////////////////////////////////
+
+// Simple Array Methods
